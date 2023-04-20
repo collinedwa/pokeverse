f4607beaf38cac54745e43b5a446a388592e671a
colledwards <colledwards@expediagroup.com>
2023-04-20 11:58:26 -0700

    added basic layout to PokemonCard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import {useState, useEffect} from 'react';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(()=>{
+    const fetchList = async () => {
+      res = await fetch(pokeApi);
+      data = await res.json();
+      setPokemonList(data.results);
+    }
+    fetchList();
+  }, []);
 
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      {pokemonList.map((pokemon)=><>
+        <PokemonCard url={pokemon.url} name={pokemon.name} />
+      </>)}
+      
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
+import {Typography, Card} from '@mui/material';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 function PokemonCard({ url, name }) {
+  const [pokeData, setPokeData] = useState({});
 
-  return (
-    <div>
-        pokemon card
-    </div>
-  );
+  useEffect(()=>{
+    const retrieve = async () => {
+      res = await fetch(url);
+      data = await res.json();
+      setPokeData(data);
+    }
+    retrieve();
+  }, []);
+
+  return <>
+    <Card>
+      <CardMedia
+        sx={{ height: 140 }}
+        //image={pokeData.sprites.front_default}
+        title={name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {/* <ul>
+            {pokeData.abilities.map((a)=> <>
+            <li>{a}</li>
+            </>)}
+          </ul> */}
+        </Typography>
+      </CardContent>
+    </Card>
+  </>
 }
 
 export { PokemonCard };
