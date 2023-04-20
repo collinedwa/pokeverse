09f8820346071a6149a4429832f68904bf68a57b
colledwards <colledwards@expediagroup.com>
2023-04-20 13:22:55 -0700

    fixed rendering issue; added search feature and improved look

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import { Box, TextField, Button } from '@mui/material';
 import {useState, useEffect} from 'react';
 
 const LIMIT = 150;
@@ -8,22 +9,55 @@ const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(()=>{
     const fetchList = async () => {
-      res = await fetch(pokeApi);
-      data = await res.json();
+      const res = await fetch(pokeApi);
+      const data = await res.json();
       setPokemonList(data.results);
+      setFilteredList(data.results);
     }
     fetchList();
   }, []);
 
+  function handleChange(e) {
+    const value = e.target.value;
+    const regex = new RegExp(value, 'gi');
+    const filtered = pokemonList.filter((pokemon) => {
+      return pokemon.name.match(regex);
+    });
+
+    setFilteredList(filtered);
+  }
+
   return (
     <div data-testid="app">
       <Navigation />
-      {pokemonList.map((pokemon)=><>
-        <PokemonCard url={pokemon.url} name={pokemon.name} />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: "column",
+          justifyContent: 'center',
+          maxWidth: 200,
+          mx: "auto"
+      }}
+      >
+      <TextField onChange={(e)=>handleChange(e)} label="Pokemon Search" sx={{mx: "auto"}}/>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          m: 5,
+          borderRadius: 2
+        }}
+      >
+      {filteredList.map((pokemon)=><>
+        <PokemonCard url={pokemon.url} name={pokemon.name} list={filteredList} />
       </>)}
+      </Box>
       
     </div>
   );
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,36 +4,41 @@ import {Typography, Card} from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 
-function PokemonCard({ url, name }) {
-  const [pokeData, setPokeData] = useState({});
+function PokemonCard({ url, name, list}) {
+  const [pokeData, setPokeData] = useState();
 
   useEffect(()=>{
     const retrieve = async () => {
-      res = await fetch(url);
-      data = await res.json();
+      const res = await fetch(url);
+      const data = await res.json();
       setPokeData(data);
     }
     retrieve();
-  }, []);
+  }, [list]);
+
+  if (pokeData === undefined) return null;
 
   return <>
-    <Card>
+    <Card sx={{
+      width: 400,
+      height: 400,
+      m: 1
+      }}> 
       <CardMedia
-        sx={{ height: 140 }}
-        //image={pokeData.sprites.front_default}
+        sx={{ height: 200 }}
+        image={pokeData.sprites.front_default}
         title={name}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {/* <ul>
-            {pokeData.abilities.map((a)=> <>
-            <li>{a}</li>
-            </>)}
-          </ul> */}
+        <Typography variant="body1" color="text.primary">
+          abilities:
         </Typography>
+        {pokeData.abilities.map((a) => <>
+        <Typography variant="body2" color="text.secondary">{a.ability.name}</Typography>
+        </>)}
       </CardContent>
     </Card>
   </>
